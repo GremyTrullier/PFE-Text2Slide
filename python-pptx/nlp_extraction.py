8c671dbaae9971425f30a3b7108e2458ad9839ac
GremyTrullier <32458664+GremyTrullier@users.noreply.github.com>
2021-02-15 14:19:03 +0100

    added nlp functions from Kenny to nlp extraction functions

--- python-pptx/nlp_extraction.py
+++ python-pptx/nlp_extraction.py
@@ -1,8 +1,17 @@
 #importing libraries
 # https://blog.floydhub.com/gentle-introduction-to-text-summarization-in-machine-learning/
+import nltk
+
+from nltk import pos_tag
+from rake_nltk import Rake
+from nltk.corpus import wordnet
 from nltk.corpus import stopwords
+from collections import defaultdict
 from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from gensim.summarization import keywords
 from nltk.tokenize import word_tokenize, sent_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
 
  ########################################################################
 #    Functions extracting sentences with importance above a threshold    #
@@ -75,7 +84,11 @@ def _get_article_summary(sentences, sentence_weight, threshold):
     return article_summary
 
 def _run_article_summary(article):
-    
+    """
+    Function that extract sentences above a calculated threshold.
+    Parameters:
+        - article: string with your text
+    """
     #creating a dictionary for the word frequency table
     frequency_table = _create_dictionary_table(article)
 
@@ -93,3 +106,64 @@ def _run_article_summary(article):
 
     return article_summary
 
+ ######################
+#    Using Rake Lib    #
+ ######################
+
+def extract_kw_rake(article, n=5):
+    """
+    Function that extract keywords from some text
+    Parameters:
+        - article: string with your text
+        - n (optional int): number of keywords extracted
+    """
+    r = Rake()
+    r.extract_keywords_from_text(article)
+    return "\n".join(r.get_ranked_phrases()[0:n])
+
+ #######################
+#    Using gensim lib   #
+ #######################
+
+def keywords_gensim(article, ratio=False, n=5):
+    """
+    Function that extract keywords from some text
+    Parameters:
+        - article: string with your text
+        - ratio (optional bool): if true, the function will return 20% of the keywords
+        - n (optional int): number of keywords extracted
+    """
+    if ratio:
+        return keywords(article, ratio, lemmatize=True)
+    return keywords(article, words=n, lemmatize=True)
+
+ ###################################
+#    Using nltk and sklearn libs    #
+ ###################################
+
+def extract_nltk_sklearn(article):
+    """
+    Function that extract keywords from some text
+    Parameters:
+        - article: string with your text
+    """
+    tokenizer = nltk.RegexpTokenizer(r"\w+")
+    text = tokenizer.tokenize(article.lower())
+    stopWords = stopwords.words('english')
+    text = [w for w in text if not w in stopWords]
+    tag_map = defaultdict(lambda: wordnet.NOUN)
+    tag_map['J'] = wordnet.ADJ
+    tag_map['V'] = wordnet.VERB
+    tag_map['R'] = wordnet.ADV
+
+    lemmatizer = WordNetLemmatizer()
+    words=[]
+
+    for token, tag in pos_tag(text):
+        words.append(lemmatizer.lemmatize(token, tag_map[tag[0]]))
+
+    text = words
+    vectorizer = TfidfVectorizer()
+
+    X = vectorizer.fit_transform(text)
+    return "\n".join(vectorizer.get_feature_names())
